abcfd7d7a28b7758c91df1709446bc6393dbd3c0
Tay Buley <buley@outlook.com>
2026-01-13 13:30:22 -0800

    docs: update branding to affectively.ai and remove emojis

---
+++ target/wasm32-unknown-unknown/release/deps/affectively_graph_algorithms.d
@@ -0,0 +1,11 @@
+E:\emotions\open-source\wasm-graph-algorithms\target\wasm32-unknown-unknown\release\deps\affectively_graph_algorithms.d: src\lib.rs src\dag.rs src\topological_sort.rs src\cycle_detection.rs src\path_finding.rs
+
+E:\emotions\open-source\wasm-graph-algorithms\target\wasm32-unknown-unknown\release\deps\affectively_graph_algorithms.wasm: src\lib.rs src\dag.rs src\topological_sort.rs src\cycle_detection.rs src\path_finding.rs
+
+E:\emotions\open-source\wasm-graph-algorithms\target\wasm32-unknown-unknown\release\deps\libaffectively_graph_algorithms.rlib: src\lib.rs src\dag.rs src\topological_sort.rs src\cycle_detection.rs src\path_finding.rs
+
+src\lib.rs:
+src\dag.rs:
+src\topological_sort.rs:
+src\cycle_detection.rs:
+src\path_finding.rs:
